abf16ff92cd83d7b1770501d62000ddd7126f281
shagaranasution <shagaranst@gmail.com>
2025-08-14 10:06:57 +0700

    Create Signin screen UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,5 +31,5 @@ export default function RootLayout() {
 
   if (!fontsLoaded) return null;
 
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,52 @@
+import icons from '@/constants/icons';
+import images from '@/constants/images';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SignIn() {
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-8">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To ReState
+          </Text>
+
+          <Text className="text-center text-3xl font-rubik-bold text-black-300 mt-2">
+            Let&apos;s Get You Closer{'\n'}To{' '}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-10">
+            Login To ReState with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-4">
+            <View className="flex flex-row justify-center items-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
